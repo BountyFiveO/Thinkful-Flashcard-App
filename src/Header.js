91401501f168fdecac0eec5e6f054d59888e3e65
S. Patterson <bountyfiveo@gmail.com>
2022-12-13 17:50:37 -0500

    deleted logo

--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,6 @@ import "./style.css";
 function Header() {
 	return (
 		<header className="jumbotron bg-dark">
-			<a href="https://engineerpatterson.com">
-				<img
-					src="https://engineerpatterson.com/images/ep-logo.png"
-					alt="Engineer Patterson Logo"
-					className="ep-logo"
-					// width="428px"
-					// height="100px"
-				/>
-			</a>
 			<div className="container text-white">
 				<h1 className="display-4">Flashcard Study App</h1>
 				<p className="lead">by Engineer Patterson</p>
