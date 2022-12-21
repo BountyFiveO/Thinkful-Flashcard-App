f3b48e55cdb9977444c1c54c51408ccf9bab6fbe
S. Patterson <bountyfiveo@gmail.com>
2022-12-21 14:52:53 -0500

    Update index.js

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,8 +2,8 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-// const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
-const API_BASE_URL = process.env.API_BASE_URL || "https://flashcard.engineerpatterson.com";
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
+
 
 /**
  * Defines the default headers for these functions to work with `json-server`
