d57ae22610502eeadf64596c50a85579b27adc17
S. Patterson <bountyfiveo@gmail.com>
2023-01-12 00:59:47 -0500

    updates

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 // -------------START IMPORTS-------------
 import React from "react";
+import "./style.css";
 // -------------END IMPORTS-------------
 
 // -------------START COMPONENT-------------
@@ -9,16 +10,16 @@ function Header() {
 		// this is the header for the app and it contains the logo and the title
 		<header className="jumbotron bg-dark">
 			<div className="logo-container">
-			<a href="https://engineerpatterson.com"><img src="https://engineerpatterson.com/images/ep-logo.png"
+			</div>
+			<div className="container text-white">
+				<h1 className="display-4">Flashcard Study App</h1>
+				<p className="lead">by Engineer Patterson</p>
+				<a href="https://engineerpatterson.com"><img src="https://engineerpatterson.com/images/ep-logo.png"
 					alt="Engineer Patterson Logo"
 					className="ep-logo"
 					width="428px"
 					height="100px"
 				/></a>
-			</div>
-			<div className="container text-white">
-				<h1 className="display-4">Flashcard Study App</h1>
-				<p className="lead">by Engineer Patterson</p>
 			</div>
 		</header>
 	);
