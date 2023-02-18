3909efa7c9679e63411d8e8a170b53004578e6b9
S. Patterson <bountyfiveo@gmail.com>
2023-02-17 22:33:55 -0500

    Update knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,7 +13,7 @@ const {
   module.exports = {
     production: {
        client: "postgresql",
-       connection: PRODUCTION_DATABASE_URL,
+       connection: "postgres://lgsltxdw:D-avIYxoQWGCpYTg6P4XOvIiSEP-LAgX@jelani.db.elephantsql.com/lgsltxdw",
        pool: { min: 0, max: 5 },
        migrations: {
          directory: path.join(__dirname, "src", "data", "migrations"),
