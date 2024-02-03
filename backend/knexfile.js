23dd59d7768c04bd22d66922b954d1599724c701
S. Patterson <bountyfiveo@gmail.com>
2024-02-03 09:35:22 -0600

    Update knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,7 +13,7 @@ const {
   module.exports = {
     production: {
        client: "postgresql",
-       connection: "postgres://lgsltxdw:D-avIYxoQWGCpYTg6P4XOvIiSEP-LAgX@jelani.db.elephantsql.com/lgsltxdw",
+       connection: "postgres://liktgtfn:M--djEMSlCQefiQQtXmPgQckdqyGQTZR@chunee.db.elephantsql.com/liktgtfn",
        pool: { min: 0, max: 5 },
        migrations: {
          directory: path.join(__dirname, "src", "data", "migrations"),
